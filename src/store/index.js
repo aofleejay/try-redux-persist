2a56f93fd0c1cb2f6ee547fea2f88902abaa2368
kunapot <kunapot@thinknet.co.th>
2018-02-22 18:58:47 +0700

    remove code to base code

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,6 @@
 import { createStore } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
 import rootReducer from '../reducers'
 
-const persistConfig = {
-  key: 'root',
-  storage,
-}
+const store = createStore(rootReducer)
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-export default () => {
-  let store = createStore(persistedReducer)
-  let persistor = persistStore(store)
-  return { store, persistor }
-}
+export default store
